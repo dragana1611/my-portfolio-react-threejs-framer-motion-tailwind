0017236f96fb74c09ec70bc3f18f73e09789e373
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-10-12 16:24:29 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './assets/images/dplogo_cyrcle.png'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className='logo'><img src={logo} alt="logo" /></div>
+        <h1 className='section-title'>
+          Hello world!
+        </h1>
+        <nav>
+          nav
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      primary: "Poppins ",
+      body: "Poppins",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "3rem",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#ffffff",
+        tertiary: "#000000",
+      },
+    },
+  },
+  plugins: [],
+};
