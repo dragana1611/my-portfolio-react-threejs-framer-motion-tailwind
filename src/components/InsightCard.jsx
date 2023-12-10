918f89297a85ac21d69b28a04a1e09d5ba593add
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-12-10 23:10:15 +0100

    fix responsive

--- src/components/InsightCard.jsx
+++ src/components/InsightCard.jsx
@@ -5,16 +5,18 @@ import { fadeIn } from "../utils/motion";
 const InsightCard = ({ imgUrl, text, index }) => (
   <motion.div
     variants={fadeIn("up", "spring", index * 0.5, 1)}
-    className='w-full flex'
+    className="w-full flex"
   >
-    <div className='w-full flex md:flex-row flex-col gap-2 justify-between items-start'>
-    <img
-      src={imgUrl}
-      alt='check'
-      className='w-[24px] h-[24px] md:w-[36px] md:h-[36px] object-cover'
-    />    
-      <div className='flex-1 w-full'>
-        <p className='font-normal lg:text-lg text-base'>{text}</p>
+    <div className="w-full flex md:flex-row flex-col gap-2 justify-between items-start">
+      <img
+        src={imgUrl}
+        alt="check"
+        className="w-[24px] h-[24px] md:w-[36px] md:h-[36px] object-cover"
+      />
+      <div className="flex-1 w-full">
+        <p className="font-normal xl:text-[1.25rem] lg:text-[1rem] text-base xl:leading-8">
+          {text}
+        </p>
       </div>
     </div>
   </motion.div>
